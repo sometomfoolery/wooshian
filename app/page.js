28f190efc05dd02029c4751a7c2c16612a112289
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-02 10:38:44 -0400

    Changing up front page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={styles.main}>
       <p>Wooshian</p>
     </div>
   );
