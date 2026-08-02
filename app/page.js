af61bdee7fa79d5b2965317d0708b358de0b7339
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-02 17:39:39 -0400

    Removed main

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
         <p>Wooshian</p>
-      </main>
     </div>
   );
 }
