df87fab41d8e69a99cf169ef26b20e3d0e5ef2b2
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-09 18:02:54 -0400

    Added subpage

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { getSession } from "@/lib/auth";
 import LoginForm from "@/components/LoginForm";
---
+++ app/subpage/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default async function Home() {
+    return (
+        <div className={styles.page}>
+            <p className={styles.title}>Subpage</p>
+        </div>
+    );
+}
