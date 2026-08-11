1e6dc4f26c528a4ea9f8288185eb175ec48d0238
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-11 19:35:05 -0400

    Little things

--- app/game-library/page.js
+++ app/game-library/page.js
@@ -5,10 +5,10 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 
 async function GameLibraryData() {
-    const supabase = await createClient(supabaseUrl, supabasePublishableKey);
+    const supabase = createClient(supabaseUrl, supabasePublishableKey);
     const { data: games, error } = await supabase.from("games").select("*");
     if (error) {
-        console.error("Error fetching games: ", error);
+        console.log("Error fetching games: ", error);
         return <p>Error fetching games.</p>;
     }
     return <pre>{JSON.stringify(games, null, 2)}</pre>;
