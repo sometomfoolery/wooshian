ebd1745157c8a74e3c6ff54d7f970fd9f3da33a2
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-11 19:09:30 -0400

    Connected game library

--- app/game-library/page.js
+++ app/game-library/page.js
@@ -1,7 +1,19 @@
+import createClient from "@/lib/supabase-server";
+import { Suspense } from "react";
+
+async function GameLibraryData() {
+    const supabase = await createClient();
+    const { data: games } = await supabase.from("games").select("*");
+    return games;
+}
+
 export default async function Home() {
     return (
         <div>
             <h1>Game Library</h1>
+            <Suspense fallback="Loading...">
+                <GameLibraryData />
+            </Suspense>
         </div>
     );
 }
