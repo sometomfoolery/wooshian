9b684995d47f621ea4a0a2261947b1aaaef452aa
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-12 19:28:47 -0400

    Updated file name
    style.css to style.module.css

--- app/game-library/page.js
+++ app/game-library/page.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { Suspense } from "react";
-import styles from "./style.css";
+import styles from "./style.module.css";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
