ac2c418ac62bf9a26f859e83cfa90f5b5ce5dee8
SomeTomfoolery <tomthebomb.tawood@gmail.com>
2026-08-16 17:34:02 -0400

    Added header to HTML and CSS
    Added a header to the layout file in the root app field. Added an header id to the css file along with the AI suggested css properties. I already suspect those css properties won't transfer with the layout and will need to be moved to global

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import styles from "./page.module.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <div id={styles.header}></div>
+        {children}
+      </body>
     </html>
   );
 }
